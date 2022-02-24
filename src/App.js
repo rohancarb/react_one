cdbefd3bbd0cf8ca06fac5266b96effdaf51ea5c
Nitish Bhartiya <rohan.carb@gmail.com>
2022-02-24 13:41:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import AddUser from './components/Users/AddUser';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <AddUser />
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Card from '../UI/Card';
+import Button from '../UI/Button';
+import classes from './AddUser.module.css';
+
+const AddUser = props => {
+    const addUserHandler = (event) => {
+        event.preventDefault();
+    }
+    return(
+        <Card className = {classes.input}>
+    <form onSubmit={addUserHandler}>
+        <label htmlFor="username">username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <Button type="submit">Add User</Button>        
+    </form>
+        </Card>
+
+    )
+};
+
+
+export default AddUser;
